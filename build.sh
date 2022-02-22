e96a488a4acec887b8d179bf16a9a6203f82a5b5
Adam Spofford <adam.spofford@dfinity.org>
2022-02-22 12:08:25 -0800

    Add a better message

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 set -e
 cargo build --release --package nft-wallet --target wasm32-unknown-unknown
 if ! command -v ic-cdk-optimizer &> /dev/null; then
-    cargo install --root target ic-cdk-optimizer
-    PATH="$PATH:$PWD/target/bin"
+    echo 'ic-cdk-optimizer is not installed; installing it locally. Install it globally to skip this step'
+    echo 'This may take a while'
+    cargo install --root target ic-cdk-optimizer --version 0.3.4 2> /dev/null
 fi
 cd target/wasm32-unknown-unknown/release
 ic-cdk-optimizer nft_wallet.wasm -o nft_wallet-opt.wasm
