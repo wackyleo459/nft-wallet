bc802d603a7b33c880d9fd7e04917b8a2e346373
Sue Ann Kim <wackyleo459@gmail.com>
2022-03-25 12:15:19 -0700

    Login default page

--- frontend/src/pages/WalletView.svelte
+++ frontend/src/pages/WalletView.svelte
@@ -2,16 +2,22 @@
     import NFTGrid from '../components/NFTGrid.svelte';
     import { fetchAllOwnedNfts, isAuthorized } from '../nft.js';
     import {loadSpinner, hideSpinner} from '../components/Loader.svelte';
-
+    import Button, { Label } from '@smui/button';
+    import { login } from '../components/Authenticator.svelte'
 </script>
 <div class="wallet-view">
     {#await isAuthorized() then isAuthorized}
     {#if !isAuthorized}
-        <div class="jumbotron">
-            <h2>
-                Welcome to the nft wallet for the IC
-            </h2>
-            The wallet can hold countless NFTs, all in a single secure wallet! Start adding your NFTs here. Once in your wallet, you can view, send, receive NFTs, and more. Please login first.
+        <h2>
+            Welcome to the nft wallet for the IC
+        </h2><br/>
+        The wallet can hold countless NFTs, all in a single secure wallet! <br/>Start adding your NFTs here. Once in your wallet, you can view, send, receive NFTs, and more!"
+        <div class="action">
+            Please login
+            <br><br>
+            <Button variant="raised" on:click={login}>
+                <Label>LOGIN</Label>
+            </Button>
         </div>
     {/if}
     {#if isAuthorized}
@@ -30,21 +36,26 @@
 <style>
     .wallet-view {
         width: 100%;
-    }
-    .jumbotron {
-        border: 2px solid #6d2482;
-        background-color: #4f1c5e;
         line-height: 1.5rem;
         padding: 3em;
         display: flex;
         flex-direction: column;
         border-radius: 15px;
-        font-size: 14px;
+        font-size: 16px;
+        justify-content: center;
     }
     .button {
         height: 3em;
         align-items: center;
         display: flex;
         margin: 2em;
+    }
+    .action {
+        height: 300px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.5em;
     }
 </style>
