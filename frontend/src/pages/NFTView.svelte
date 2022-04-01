a09c704a6d5d0d85c38c5fbe44a5a59a808a84bd
Sue Ann Kim <wackyleo459@gmail.com>
2022-04-01 11:06:51 -0700

    layout tmplate NFT View

--- frontend/src/pages/NFTView.svelte
+++ frontend/src/pages/NFTView.svelte
@@ -4,12 +4,13 @@
     import LocationTypeIcon from '../components/LocationTypeIcon.svelte';
     import mime from 'mime/lite';
     import { isAuthorized } from '../nft';
-    import Card, {
+    import {
         Content,
-        PrimaryAction,
         Media,
         MediaContent,
     } from '@smui/card';
+    import Button, { Label } from '@smui/button';
+    import { Tile, CopyButton } from "carbon-components-svelte";
     export let nft;
     export let current;
     $:current = current || 0;
@@ -18,85 +19,95 @@
 </script>
 
 <div class="NFT_view">
-    <Card>
-        <div class="badge">NFT</div>
-        <h2>
-            {nft.symbol} #{nft.index}
-        </h2>
-        <Media class="card-media-16x9">
-            <MediaContent>
-            <Carousel content={nft.content} bind:current --fallback-bg="gray">
-                <svelte:fragment slot="fallback" let:contentType let:src>
-                    {@const extension = mime.getExtension(contentType)}
-                    {@const filename = extension ? `${nft.symbol}_${current}.${extension}` : `${nft.symbol}_${current}`}
-                    <a class="button" href={src} download={filename}>Download {extension.toUpperCase() || 'file'}</a>
-                </svelte:fragment>
-            </Carousel>
-            </MediaContent>
-        </Media>
-    </Card>
-    <Card>
-        <p>
+    <div id="tile">
+        <Tile istyle="max-width: 350px; margin:auto;">
+            <div class="badge">NFT</div>
+            <h2>
+                {nft.symbol} #{nft.index}
+            </h2>
+            <Media >
+                <MediaContent style="position:relative" id="media_content">
+                <Carousel content={nft.content} bind:current --fallback-bg="gray">
+                    <svelte:fragment slot="fallback" let:contentType let:src>
+                        {@const extension = mime.getExtension(contentType)}
+                        {@const filename = extension ? `${nft.symbol}_${current}.${extension}` : `${nft.symbol}_${current}`}
+                        <a class="button" href={src} download={filename}>Download {extension.toUpperCase() || 'file'}</a>
+                    </svelte:fragment>
+                </Carousel>
+                </MediaContent>
+            </Media>
+        </Tile>
+    </div>
+    <div class="nft_info">
+        <div class="info_title">Minted Date</div>
+        <hr>
+        <div class="info_detail">12/25/21</div>
+        <div class="info_title">Type</div>
+        <hr>
+        <div class="info_detail">
             <img src={nft.icon} alt="{nft.name} NFT icon" class="icon">
-            {nft.name}
+                {nft.name}
             {#if locationType}
             <a href={nft.location}><LocationTypeIcon {locationType}/></a>
             {/if}
-        </p>
-        <p class="canister"><Copier always text={nft.canister}>{nft.canister}</Copier></p>
-        {#await isAuthorized() then isAuthorized}
-        {#if isAuthorized}
-        <a class="transfer_button" href="/{nft.canister}/{nft.index}/transfer">Transfer</a>
-        {/if}
-        {/await}
+        </div>
 
-    </Card>
+        <div class="info_title">Canister ID</div>
+        <hr>
+        <div class="info_detail">
+            {nft.canister}
+           <CopyButton text={nft.canister} feedback="Copied to clipboard" style="left:5px"></CopyButton>
+        </div>
+        <div id="action_calls">
+            {#await isAuthorized() then isAuthorized}
+            {#if isAuthorized}
+            <Button id="button" variant="raised" style="width:100%" href="/{nft.canister}/{nft.index}/transfer">
+                <Label>Transfer</Label>
+            </Button>
+            {/if}
+            {/await}
+        </div>
+    </div>
 </div>
 
 <style>
-    div {
-        display: flex;
-        justify-items: center;
-        flex-direction: column;
-        /* width: 330px; */
+    .NFT_view {
         max-height: 80vh;
     }
-    p {
-        font-size: 20px;
-        text-align: center;
+    #tile {
+        margin: 1em;
     }
-    .canister {
-        font-size: 1em;
-        text-align: center;
+    @media (min-width: 630px) {
+        .NFT_view {
+            display: grid;
+            grid-template-areas: "picture info";
+        }
+        #tile {
+            grid-area: picture;
+        }
+        .nft_info {
+            grid-area: info;
+        }
+    }
+    .info_title {
+        font-size: 18px;
+    }
+    .info_detail {
+        margin-bottom: 2em;
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
     }
     .button {
         position: relative;
         margin: auto;
     }
-    .transfer_button {
-        border: solid 2px #5f2684;
-        margin: 5px;
-        padding: 6px 8px;
-        border-radius: 5px;
-        text-align: center;
-        margin: 15px 0;
-    }
-    .transfer_button:hover {
-        background-color: #5f2684;
-        color: white;
-    }
-    a:link { text-decoration: none; }
-    a:visited { text-decoration: none; }
-    a:hover { text-decoration: none; }
-    a:active { text-decoration: none; }
-
     h2 {
         font-family: 'Noto Sans', sans-serif;
         font-family: 'Roboto Mono', monospace;
     }
     .badge {
         font-size: 18px;
-        width: 2em;
         display: inline-block;
         padding: 0.35em 0.65em;
         font-weight: 700;
@@ -107,5 +118,11 @@
         border-radius: 0.25rem;
         background-color: #ffc107;
         display: inline-block;
+    }
+    .nft_info {
+        padding: 1em;
+    }
+    #action_calls {
+        padding: 2em 0;
     }
 </style>
